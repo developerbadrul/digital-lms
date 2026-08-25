b88ebef2072e5e44f0d60df3feede1e3cc05929a
Md Badrul Alam <developer.badrul@gmail.com>
2026-08-25 21:34:34 +0600

    add enrollments query

---
+++ queries/enrollments.js
@@ -0,0 +1,18 @@
+import { leanSanitize } from "@/lib/mongo-utils";
+import { Enrollment } from "@/model/enrollment-model";
+import mongoose from "mongoose";
+
+export async function getEnrollmentsForCourse(courseId) {
+    if (!courseId) return [];
+
+    try {
+        const validCourseId = typeof courseId === "string" && mongoose.Types.ObjectId.isValid(courseId)
+            ? new mongoose.Types.ObjectId(courseId) : courseId;
+
+        const enrollments = await Enrollment.find({ course: validCourseId }).lean();
+        return leanSanitize(enrollments)
+    } catch (error) {
+        console.error("Failed to fetch enrollments for course:", error);
+        return [];
+    }
+}
